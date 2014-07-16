71f7ca3144f0c2f447b26cb9f890de358a73af8c
MDCollins <matt@m-collins.com>
2014-07-15 20:04:35 -0400

    Cleaned up some obsolete imports. Added a README.md and a LICENSE.txt. Release the Kraken!

---
+++ README.md
@@ -0,0 +1,6 @@
+This project may not work with the latest Bukkit version (or at all) and may not receive any further updates by myself however, feel free to fork this and make improvements. I'll certainly consider any pull requests made and will try to assist with any issues.
+
+#PVPKarma
+
+What it was:
+- An interesting experiment on creating a "karma" for players based on killing other players or mobs
--- src/main/java/com/blockmovers/plugins/warmcoolings/Main.java
+++ src/main/java/com/blockmovers/plugins/warmcoolings/Main.java
@@ -1,12 +1,9 @@
 package com.blockmovers.plugins.warmcoolings;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 import java.util.logging.Logger;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
